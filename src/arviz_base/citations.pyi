bd393689266dfec5724093dd25af799666107cd2
aloctavodia <aloctavodia@gmail.com>
2025-06-26 13:00:04 +0300

    add function to retrive citations for arviz or methods implemented in arviz

---
+++ src/arviz_base/citations.pyi
@@ -0,0 +1,10 @@
+# File generated with docstub
+
+import os
+import re
+from typing import List
+
+def citations(
+    methods: List | None = ..., filepath: str | None = ..., format_type: str = ...
+) -> None: ...
+def _citation_bibtex(content, keys) -> None: ...
